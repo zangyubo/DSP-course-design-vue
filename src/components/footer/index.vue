ddb287861332ff85f218dcb23c9ed6139924102d
ZangYubo <129265134+zangyubo@users.noreply.github.com>
2024-11-01 22:22:30 +0800

    Initial commit

---
+++ src/components/footer/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-layout-footer class="footer">
+    Copyright by @zangyubo 2024
+  </a-layout-footer>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped>
+  .footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+    color: var(--color-text-2);
+    text-align: center;
+  }
+</style>
